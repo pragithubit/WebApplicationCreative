a263ca24388b4315e8b0c7b005ffd0c40b7c1b8f
pradeepsahoo <pradeepsahoo@PRADEEP-SAHOO56>
2024-08-08 11:17:25 +0530

    Add project files.

---
+++ WebApplicationCreative/CreativePage.aspx
@@ -0,0 +1,158 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreativePage.aspx.cs" Inherits="WebApplicationCreative.CreativePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 260px;
+        }
+        .search-container {
+    display: flex;
+    align-items: center;
+}
+ .search-textbox {
+    flex-grow: 0; /* This makes the textbox take the remaining space */
+}
+
+.search-button {
+    margin-left: 10px; /* Adds some space between the textbox and the button */
+    background-color: #ff0066;
+    border: none;
+    color: white;
+    padding: 8px 16px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    cursor: pointer;
+}
+.search-button1{
+    margin-right:10px;
+    background-color:blue;
+    color:white;
+    padding:8px 16px;
+    text-align:center;
+    display:inline-block;
+    font-size:16px;
+    cursor:pointer;
+}
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2">ProductId</td>
+                    <td>
+                        <asp:TextBox ID="txtProductId" runat="server" Width="229px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">ProductName</td>
+                    <td>
+                        <asp:TextBox ID="txtProductName" runat="server" Width="238px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">ProductDescription</td>
+                    <td>
+                        <asp:TextBox ID="txtProductDescription" runat="server" Width="232px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">CategoryName</td>
+                    <td>
+                        <asp:DropDownList ID="DropDownList1Category" runat="server">
+                             <asp:ListItem Value="" Text="Select Item"></asp:ListItem>
+                            <asp:ListItem Value="1" Text="Electronics"></asp:ListItem>
+                            <asp:ListItem Value="2" Text="Mobile"></asp:ListItem>
+                            <asp:ListItem Value="3" Text="Cloth"></asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">UserName</td>
+                    <td>
+                        <asp:TextBox ID="txtUserName" runat="server" Width="229px"></asp:TextBox>
+                    </td>
+                </tr>
+            </table>
+            <asp:Label runat="server" ID="IblMsg" ForeColor="Green"></asp:Label>
+
+        
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" Width="169px" OnClick="btnSubmit_Click" />
+    </div>
+        <br /><br />
+        <div class="search-container" >
+            <asp:TextBox ID="txtSearch" runat="server" placeholder="Enter ProductId" CssClass="search-textbox"></asp:TextBox>
+            <asp:Button ID="btnSearch" runat="server" BackColor="#ff0066" CssClass="search-button" Text="Search" OnClick="btnSearch_Click" />
+            <asp:Button ID="btnReset" runat="server" BackColor="#3366ff" CssClass="search-button" Text="Reset" OnClick="btnReset_Click" />
+        </div>
+ <div>
+
+
+           <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" DataKeyNames="ProductId" OnRowEditing="GridView1_RowEditing"
+                OnRowUpdating="GridView1_RowUpdating" 
+                OnRowCancelingEdit="GridView1_RowCancelingEdit" 
+                OnRowDeleting="GridView1_RowDeleting" OnRowDataBound="GridView1_RowDataBound">
+               <Columns>
+                <asp:BoundField DataField="ProductId" HeaderText="Product ID" ReadOnly="True" />
+                
+                <asp:TemplateField HeaderText="Product Name">
+                    <ItemTemplate>
+                        <%# Eval("ProductName") %>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtProductName" runat="server" Text='<%# Eval("ProductName") %>' />
+                    </EditItemTemplate>
+                </asp:TemplateField>
+                
+                <asp:TemplateField HeaderText="Product Description">
+                    <ItemTemplate>
+                        <%# Eval("ProductDescription") %>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtProductDescription" runat="server" Text='<%# Eval("ProductDescription") %>' />
+                    </EditItemTemplate>
+                </asp:TemplateField>
+                
+                <asp:TemplateField HeaderText="Category Name">
+                    <ItemTemplate>
+                        <%# Eval("CategoryName") %>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:DropDownList ID="ddlCategoryName" runat="server">
+                             <asp:ListItem Value="" Text="Select Item"></asp:ListItem>
+                      <asp:ListItem Value="1" Text="Electronics"></asp:ListItem>
+                        <asp:ListItem Value="2" Text="Mobile"></asp:ListItem>
+                         <asp:ListItem Value="3" Text="Cloth"></asp:ListItem>
+                        </asp:DropDownList>
+                    </EditItemTemplate>
+                </asp:TemplateField>
+                
+                <asp:TemplateField HeaderText="User Name">
+                    <ItemTemplate>
+                        <%# Eval("UserName") %>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtUserName" runat="server" Text='<%# Eval("UserName") %>' />
+                    </EditItemTemplate>
+                </asp:TemplateField>
+                
+                <asp:CommandField ShowEditButton="true" ShowDeleteButton="true" />
+            </Columns>
+            </asp:GridView>
+
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ WebApplicationCreative/CreativePage.aspx.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationCreative
+{
+    public partial class CreativePage : System.Web.UI.Page
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["CreateDBConnectionString"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridView();
+                LoadCategories();
+            }
+        }
+
+        private void BindGridView(string searchTerm = null)
+        {
+           
+
+            string query;
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                query = @"
+            SELECT 
+                p.ProductId, 
+                p.ProductName, 
+                p.ProductDescription, 
+                p.UserName, 
+                p.CategoryId, 
+                c.CategoryName 
+            FROM  
+                Product p  
+            INNER JOIN 
+                Category c  
+            ON 
+                p.CategoryId = c.CategoryId";
+            }
+            else
+            {
+                query = @"
+            SELECT 
+                p.ProductId, 
+                p.ProductName, 
+                p.ProductDescription, 
+                p.UserName, 
+                p.CategoryId, 
+                c.CategoryName 
+            FROM  
+                Product p  
+            INNER JOIN 
+                Category c  
+            ON 
+                p.CategoryId = c.CategoryId 
+            WHERE 
+                p.ProductId = @ProductId";
+            }
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    if (!string.IsNullOrEmpty(searchTerm))
+                    {
+                        cmd.Parameters.AddWithValue("@ProductId", searchTerm);
+                    }
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        GridView1.DataSource = dt;
+                        GridView1.DataBind();
+                    }
+                }
+            }
+
+        }
+        private void LoadCategories()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["CreateDBConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetCategories", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    DropDownList1Category.DataSource = reader;
+                    DropDownList1Category.DataTextField = "CategoryName"; // Text to display in the DropDownList
+                    DropDownList1Category.DataValueField = "CategoryId";  // Value associated with the text
+                    DropDownList1Category.DataBind();
+                }
+            }
+
+            // Add a default item at the beginning
+            DropDownList1Category.Items.Insert(0, new ListItem("Select Category", "0"));
+        }
+
+
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("InsertPro", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    //cmd.Parameters.AddWithValue("@ProductId", txtProductId.Text);
+                    cmd.Parameters.AddWithValue("@ProductName", txtProductName.Text);
+                    cmd.Parameters.AddWithValue("@ProductDescription", txtProductDescription.Text);
+                    cmd.Parameters.AddWithValue("@CategoryName", DropDownList1Category.SelectedValue);
+                    cmd.Parameters.AddWithValue("@UserName", txtUserName.Text);
+                    cmd.Parameters.AddWithValue("@CategoryId",DropDownList1Category.SelectedValue);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+
+            IblMsg.Text = "Product added successfully!";
+            BindGridView();
+
+            txtProductId.Text = "";
+            txtProductName.Text = "";
+            txtProductDescription.Text = "";
+            DropDownList1Category.SelectedIndex = -1; // Reset DropDownList
+            txtUserName.Text = "";
+
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+             GridView1.EditIndex = e.NewEditIndex;
+            BindGridView();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+
+          
+            int productId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+
+            // Get the updated values from the controls
+            string productName = (row.FindControl("txtProductName") as TextBox).Text;
+            string productDescription = (row.FindControl("txtProductDescription") as TextBox).Text;
+            string userName = (row.FindControl("txtUserName") as TextBox).Text;
+            //DropDownList ddlCategoryName = row.FindControl("ddlCategoryName") as DropDownList;
+            //string categoryName = ddlCategoryName.SelectedItem.Text; // Use the selected text
+            DropDownList ddlCategoryName = row.FindControl("ddlCategoryName") as DropDownList;
+            string categoryId = ddlCategoryName.SelectedValue;
+            // Update the database
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("UpdateProductList", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    // Add parameters
+                    cmd.Parameters.AddWithValue("@ProductId", productId);
+                    cmd.Parameters.AddWithValue("@ProductName", productName);
+                    cmd.Parameters.AddWithValue("@ProductDescription", productDescription);
+                    cmd.Parameters.AddWithValue("@UserName", userName);
+                    //cmd.Parameters.AddWithValue("@CategoryName", categoryName);
+                    cmd.Parameters.AddWithValue("@CategoryId", categoryId);
+
+                    // Open the connection, execute the command, then close the connection
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+
+            // Exit edit mode and rebind the GridView to reflect the changes
+            GridView1.EditIndex = -1;
+            BindGridView();
+
+        }
+
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex= -1;
+            BindGridView();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int productId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("DeleteProduct", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ProductId", productId);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+
+            BindGridView();
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow && (e.Row.RowState & DataControlRowState.Edit) > 0)
+            {
+                DropDownList ddlCategoryName = (DropDownList)e.Row.FindControl("ddlCategoryName");
+                if (ddlCategoryName != null)
+                {
+                    // Populate the dropdown list
+                    ddlCategoryName.Items.Clear();
+                    ddlCategoryName.Items.Add(new ListItem("Select Item", ""));
+                    ddlCategoryName.Items.Add(new ListItem("Electronics", "1"));
+                    ddlCategoryName.Items.Add(new ListItem("Mobile", "2"));
+                    ddlCategoryName.Items.Add(new ListItem("Cloth", "3"));
+
+                    // Check the actual type of the data item and cast accordingly
+                    if (e.Row.DataItem is DataRowView drv)
+                    {
+                        ddlCategoryName.SelectedValue = drv["CategoryId"].ToString(); // Ensure you're using the correct field
+                    }
+                    else if (e.Row.DataItem is IDataRecord record)
+                    {
+                        // Debugging: Print available columns
+                        for (int i = 0; i < record.FieldCount; i++)
+                        {
+                            System.Diagnostics.Debug.WriteLine("Column {0}: {1}", i, record.GetName(i));
+                        }
+
+                        // Use the correct column name
+                        string correctColumnName = "CategoryId"; // Ensure you're using the correct field
+                        ddlCategoryName.SelectedValue = record[correctColumnName].ToString();
+                    }
+                    else
+                    {
+                        throw new InvalidCastException($"Unsupported type: {e.Row.DataItem.GetType().FullName}");
+                    }
+                }
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchTerm = txtSearch.Text.Trim();
+            BindGridView(searchTerm);
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            txtSearch.Text= string.Empty;
+            // BindGridView(txtSearch.Text);
+            BindGridView();
+        }
+    }
+    
+    
+}
